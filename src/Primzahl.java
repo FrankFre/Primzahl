8524d08ef28d85396a569c67f1412e11a361cd7e
Frank Freisberg <f.freisberg@telekom.de>
2020-03-29 12:13:42 +0200

    Fehler bei höheren Zahlen

--- src/Primzahl.java
+++ src/Primzahl.java
@@ -10,11 +10,11 @@ public class Primzahl {
 
 		Scanner eing = new Scanner(System.in);
 
-//		System.out.println("Ermittlung von Primzahlen nach dem Sieb des Eratosthenes ");
-//		System.out.print("Bitte geben sie den Zahlenbereich an: ");
-//		xmax = eing.nextInt();
+		System.out.println("Ermittlung von Primzahlen nach dem Sieb des Eratosthenes ");
+		System.out.print("Bitte geben sie den Zahlenbereich an: ");
+		xmax = eing.nextInt();
 
-		erzeugen(3);
+		erzeugen(xmax);
 
 	}
 
@@ -29,35 +29,44 @@ public class Primzahl {
 
 		}
 		return reihe;
-
 	}
 
 	private static void erzeugen(int xmax) {
 
-		int i;
+		int i, k;
 		int[] feld = new int[xmax + 1];
 
-		// Füllen des Arrays mit einer Reihe von Werten
-		for (i = 2; i <= xmax; i++) {
+		
+		for (i = 1; i <= xmax; i++) {					// Füllen des Arrays mit einer Zahlenreihe
 			feld[i] = i;
 		}
+		
 
-		i = 2;
+		for(i = 2; i <= xmax; i++) {				 // Abfrage, ob das Feld gelöscht wurde, Iteration durch Array
 
-		while (feld[i] > 1 && i <= xmax) { // Abfrage, ob das Feld gelöscht wurde, Iteration durch Array
-
-			for (int j = 2; j <= i; j++) { // Prüfung auf Primzahl, wenn nein - Feld mit 0 füllen
-
-				if (i % (1 + fakultaet(i - 1)) != 0) { // Prüfung auf Primzahl, wenn ja, aufs nächste Feld springen
-
-					feld[i] = 0; // leeren des Feldes einer nicht-Primzahl
-
-				}
-
+			for (int j = 2; j <= i; j++) { 					// Prüfung auf Primzahl, wenn nein - Feld mit 0 füllen
+				
 				System.out.println(Arrays.toString(feld));
-				i++;
+				
+				if ((1 + fakultaet(i - 1)) % i > 0) {		 // Prüfung auf Primzahl, wenn ja, aufs nächste Feld springen
+					
+					feld[i] = 0; 
+					
+					for(k = 2; k <= xmax; k++ ) {			// leeren aller Nicht-Primzahl-Vielfachen
+					
+						k = k * i;
+							if(k < xmax)  {
+								
+							feld[k] = 0;  						// leeren des Feldes einer nicht-Primzahl
+							
+							}
+					}
+						
+				}
+		
 			}
 		}
+		System.out.println(Arrays.toString(feld));
 	}
 
 }
